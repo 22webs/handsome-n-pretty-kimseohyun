d87895fca78bdfd7b6090735f0cda782320f451d
Seojisoo20191941 <amy0480@naver.com>
2023-06-09 04:23:47 +0900

    etc : 버튼 이름 변경

--- src/@pages/main.tsx
+++ src/@pages/main.tsx
@@ -25,7 +25,7 @@ export default function Main() {
       <St.Title>형그니와 지수수의 22웹스</St.Title>
       <St.Input type="text" placeholder="깃허브 유저 이름을 입력해주세요" onChange={handleInputUserName} />
       <St.Input type="text" placeholder="PAT를 입력해주세요" onChange={handleInputPAT} />
-      <St.Button onClick={() => handleMoveToPage('/get')}>맞팔하지 않은 친구 보러가기</St.Button>
+      <St.Button onClick={() => handleMoveToPage('/get')}>깃허브 팔로우 현황 보러가기</St.Button>
       <St.Button onClick={() => handleMoveToPage('/put')}>맞팔하지 않은 친구 팔로우하기</St.Button>
     </St.MainWrapper>
   );
