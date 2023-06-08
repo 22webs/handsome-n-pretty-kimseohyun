a66507f5de4092141b1e36ef7c6d94856eb63d47
Seojisoo20191941 <amy0480@naver.com>
2023-06-09 04:16:48 +0900

    feat : 유저 데이터 타입 추가

--- src/reocil/user.ts
+++ src/reocil/user.ts
@@ -1,6 +1,7 @@
 import { atom } from 'recoil';
+import { UserDataType } from '../types/userInfoType';
 
-export const userData = atom({
+export const userData = atom<UserDataType>({
   key: 'userData',
   default: {
     userName: '',
