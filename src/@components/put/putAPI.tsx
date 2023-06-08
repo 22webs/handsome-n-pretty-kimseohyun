838128aeb754974972835c688af78d0a4b642e33
Seojisoo20191941 <amy0480@naver.com>
2023-06-09 05:16:37 +0900

    feat : 클릭해서 배열에 담는 함수 구현

--- src/@components/put/putAPI.tsx
+++ src/@components/put/putAPI.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { followersData, followingData } from '../../reocil/followingData';
 import * as St from './style';
@@ -5,13 +6,28 @@ import * as St from './style';
 export default function PutAPI() {
   const followers = useRecoilValue<string[]>(followersData);
   const followings = useRecoilValue<string[]>(followingData);
+  const [readyToFollowData, setReadyToFollowData] = useState<string[]>([]);
+
+  const handle_selectName = (user: string) => {
+    if (readyToFollowData.includes(user)) {
+      const removedData = readyToFollowData.filter((data) => data !== user);
+      setReadyToFollowData(removedData);
+    } else {
+      setReadyToFollowData([...readyToFollowData, user]);
+    }
+  };
 
   return (
     <St.FollowerWrapper>
       <St.Title>유저 이름을 클릭해서 팔로우하거나, 모두 팔로우하기를 눌러주세요.</St.Title>
       {followers.length > 1 &&
-        followers.map((x) => {
-          if (!followings.includes(x)) return <St.NameBox key={x}>{x}</St.NameBox>;
+        followers.map((user) => {
+          if (!followings.includes(user))
+            return (
+              <St.NameBox key={user} onClick={() => handle_selectName(user)}>
+                {user}
+              </St.NameBox>
+            );
         })}
 
       <St.Button>팔로우하기</St.Button>
