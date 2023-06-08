b0edacbfbea4a7667cf1ac59f7c8ab81497d14ba
Seojisoo20191941 <amy0480@naver.com>
2023-06-09 03:23:45 +0900

    feat : 페이지 이동 함수 구현

--- src/@pages/main.tsx
+++ src/@pages/main.tsx
@@ -4,10 +4,14 @@ import * as St from './style';
 export default function Main() {
   const navigate = useNavigate();
 
+  const handleMoveToPage = (page: string) => {
+    navigate(page);
+  };
+
   return (
     <>
-      <St.Button>맞팔하지 않은 친구 보러가기</St.Button>
-      <St.Button>맞팔하지 않은 친구 팔로우하기</St.Button>
+      <St.Button onClick={() => handleMoveToPage('/get')}>맞팔하지 않은 친구 보러가기</St.Button>
+      <St.Button onClick={() => handleMoveToPage('/put')}>맞팔하지 않은 친구 팔로우하기</St.Button>
     </>
   );
 }
