29fa13789e020fe4f3c34bf9c86142dd39a08a5e
Seojisoo20191941 <amy0480@naver.com>
2023-06-09 05:53:16 +0900

    refactor : 함수명 바꾸기

--- src/@pages/main.tsx
+++ src/@pages/main.tsx
@@ -8,25 +8,25 @@ export default function Main() {
   const navigate = useNavigate();
   const [userInfo, setUserInfo] = useRecoilState<UserDataType>(userData);
 
-  const handleMoveToPage = (page: string) => {
+  const handle_moveToPage = (page: string) => {
     navigate(page);
   };
 
-  const handleInputUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handle_inputUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserInfo({ ...userInfo, userName: e.target.value });
   };
 
-  const handleInputPAT = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handle_inputPAT = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserInfo({ ...userInfo, PAT: e.target.value });
   };
 
   return (
     <St.MainWrapper>
       <St.Title>형그니와 지수수의 22웹스</St.Title>
-      <St.Input type="text" placeholder="깃허브 유저 이름을 입력해주세요" onChange={handleInputUserName} />
-      <St.Input type="text" placeholder="PAT를 입력해주세요" onChange={handleInputPAT} />
-      <St.Button onClick={() => handleMoveToPage('/get')}>깃허브 팔로우 현황 보러가기</St.Button>
-      <St.Button onClick={() => handleMoveToPage('/put')}>맞팔하지 않은 친구 팔로우하기</St.Button>
+      <St.Input type="text" placeholder="깃허브 유저 이름을 입력해주세요" onChange={handle_inputUserName} />
+      <St.Input type="text" placeholder="PAT를 입력해주세요" onChange={handle_inputPAT} />
+      <St.Button onClick={() => handle_moveToPage('/get')}>깃허브 팔로우 현황 보러가기</St.Button>
+      <St.Button onClick={() => handle_moveToPage('/put')}>맞팔하지 않은 친구 팔로우하기</St.Button>
     </St.MainWrapper>
   );
 }
