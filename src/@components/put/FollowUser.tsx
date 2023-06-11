ec6a52f1dfcea7e9afa16ca9d44b48073ce0fd7d
Geun-Oh <kandy1002@naver.com>
2023-06-11 15:07:02 +0900

    refactor: FollowUserPage 셀렉터를 사용하여 코드 수정

--- src/@components/put/FollowUser.tsx
+++ src/@components/put/FollowUser.tsx
@@ -1,39 +1,41 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { followUsers } from '../../api/put';
-import { followersData, followingData } from '../../state/atom/followingData';
+// import { followersData, followingData } from '../../state/atom/followingData';
 import { userData } from '../../state/atom/user';
 import { UserDataType } from '../../types/userDataType';
 import * as St from './style';
+import { userNotFollowing } from '../../state/selector/userNotFollowing';
 
 export default function FollowUser() {
-  const followers = useRecoilValue<string[]>(followersData);
-  const followings = useRecoilValue<string[]>(followingData);
-  const [notFollowingData, setNotFollowingData] = useState<string[]>([]);
+  // const followers = useRecoilValue<string[]>(followersData);
+  // const followings = useRecoilValue<string[]>(followingData);
+  // const [notFollowingData, setNotFollowingData] = useState<string[]>([]);
   const [readyToFollowData, setReadyToFollowData] = useState<string[]>([]);
+  const notFollowing = useRecoilValue(userNotFollowing);
   const userInfo = useRecoilValue<UserDataType>(userData);
   const { PAT } = userInfo;
   const navigate = useNavigate();
 
   //맞팔이 아닌 유저를 notFollowingData에 넣는 함수
-  const check_notFollowUsers = () => {
-    const data: string[] = [];
-    {
-      followers.length > 1 &&
-        followers.map((user) => {
-          if (!followings.includes(user) && !data.includes(user)) {
-            data.push(user);
-          }
-        });
-    }
-    setNotFollowingData(data);
-  };
+  // const check_notFollowUsers = () => {
+  //   const data: string[] = [];
+  //   {
+  //     followers.length > 1 &&
+  //       followers.map((user) => {
+  //         if (!followings.includes(user) && !data.includes(user)) {
+  //           data.push(user);
+  //         }
+  //       });
+  //   }
+  //   setNotFollowingData(data);
+  // };
 
-  useEffect(() => {
-    check_notFollowUsers();
-  }, []);
+  // useEffect(() => {
+  //   check_notFollowUsers();
+  // }, []);
 
   //버튼을 클릭해서 선택
   const handle_selectName = (user: string) => {
@@ -68,13 +70,13 @@ export default function FollowUser() {
 
   //맞팔이 아닌 사람 전체 팔로우
   const handle_FollowAll = () => {
-    notFollowingData.map((userName) => followNotFollowingUser({ userName, PAT }));
+    notFollowing.map((userName) => followNotFollowingUser({ userName, PAT }));
   };
 
   return (
     <St.FollowerWrapper>
       <St.Title>유저 이름을 클릭해서 팔로우하거나, 모두 팔로우하기를 눌러주세요.</St.Title>
-      {notFollowingData?.map((user) => {
+      {notFollowing.map((user) => {
         return (
           <St.NameBox key={user} onClick={() => handle_selectName(user)} $isSelected={check_isSelected(user)}>
             {user}
