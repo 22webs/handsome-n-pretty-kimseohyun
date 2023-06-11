6f60a493d770f83abb5ab1bcb83704adb6355c1e
Geun-Oh <kandy1002@naver.com>
2023-06-11 14:36:06 +0900

    chore: 레거시 코드 제거

--- src/api/get.ts
+++ src/api/get.ts
@@ -11,20 +11,6 @@ export async function getFollowings({ userName, PAT }: UserDataType) {
   });
 
   return filterUserName(data);
-
-  // return axios
-  //   .get<UserInfoType[]>(`https://api.github.com/users/${userName}/following`, {
-  //     headers: {
-  //       Authorization: `token ${PAT}`,
-  //     },
-  //   })
-  //   .then((res) => {
-  //     const { data } = res;
-
-  //     const followerNames = data.map((x) => x.login);
-
-  //     return followerNames;
-  //   });
 }
 
 export async function getFollowers({ userName, PAT }: UserDataType) {
@@ -35,21 +21,4 @@ export async function getFollowers({ userName, PAT }: UserDataType) {
   });
 
   return filterUserName(data);
-
-  // return axios
-  //   .get(`https://api.github.com/users/${userName}/followers`, {
-  //     headers: {
-  //       Authorization: `token ${PAT}`,
-  //     },
-  //   })
-  //   .then((res) => {
-  //     const { data } = res;
-  //     const followingNames: string[] = [];
-
-  //     for (let i = 0; i < data.length; i++) {
-  //       if (!followingNames.includes(data[i].login)) followingNames.push(data[i].login);
-  //     }
-
-  //     return followingNames;
-  //   });
 }
