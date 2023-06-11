d4f9d8f8c38b8883f3d0fa043e8b100bf9a28b7d
Geun-Oh <kandy1002@naver.com>
2023-06-11 14:35:34 +0900

    refactor: then => async/await을 활용하여 로직 재구성

--- src/api/get.ts
+++ src/api/get.ts
@@ -1,40 +1,55 @@
 import axios from 'axios';
 import { UserDataType } from '../types/userDataType';
+import UserInfoType from '../types/userInfoType';
+import filterUserName from '../utils/filterUserName';
 
-export function getFollowings({ userName, PAT }: UserDataType) {
-  return axios
-    .get(`https://api.github.com/users/${userName}/following`, {
-      headers: {
-        Authorization: `token ${PAT}`,
-      },
-    })
-    .then((res) => {
-      const { data } = res;
-      const followerNames: string[] = [];
+export async function getFollowings({ userName, PAT }: UserDataType) {
+  const { data } = await axios.get<UserInfoType[]>(`https://api.github.com/users/${userName}/following`, {
+    headers: {
+      Authorization: `token ${PAT}`,
+    },
+  });
 
-      for (let i = 0; i < data.length; i++) {
-        if (!followerNames.includes(data[i].login)) followerNames.push(data[i].login);
-      }
+  return filterUserName(data);
 
-      return followerNames;
-    });
+  // return axios
+  //   .get<UserInfoType[]>(`https://api.github.com/users/${userName}/following`, {
+  //     headers: {
+  //       Authorization: `token ${PAT}`,
+  //     },
+  //   })
+  //   .then((res) => {
+  //     const { data } = res;
+
+  //     const followerNames = data.map((x) => x.login);
+
+  //     return followerNames;
+  //   });
 }
 
-export function getFollowers({ userName, PAT }: UserDataType) {
-  return axios
-    .get(`https://api.github.com/users/${userName}/followers`, {
-      headers: {
-        Authorization: `token ${PAT}`,
-      },
-    })
-    .then((res) => {
-      const { data } = res;
-      const followingNames: string[] = [];
+export async function getFollowers({ userName, PAT }: UserDataType) {
+  const { data } = await axios.get<UserInfoType[]>(`https://api.github.com/users/${userName}/followers`, {
+    headers: {
+      Authorization: `token ${PAT}`,
+    },
+  });
 
-      for (let i = 0; i < data.length; i++) {
-        if (!followingNames.includes(data[i].login)) followingNames.push(data[i].login);
-      }
+  return filterUserName(data);
 
-      return followingNames;
-    });
+  // return axios
+  //   .get(`https://api.github.com/users/${userName}/followers`, {
+  //     headers: {
+  //       Authorization: `token ${PAT}`,
+  //     },
+  //   })
+  //   .then((res) => {
+  //     const { data } = res;
+  //     const followingNames: string[] = [];
+
+  //     for (let i = 0; i < data.length; i++) {
+  //       if (!followingNames.includes(data[i].login)) followingNames.push(data[i].login);
+  //     }
+
+  //     return followingNames;
+  //   });
 }
