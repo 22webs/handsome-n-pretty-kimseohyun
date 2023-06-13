e9bfd7a876c9a80f735483c5fb1c9a9f5471a1f1
Seojisoo20191941 <amy0480@naver.com>
2023-06-13 21:34:37 +0900

    fix : axios로 안되던 문제 해결

--- src/api/put.ts
+++ src/api/put.ts
@@ -1,27 +1,11 @@
-// export async function followUsers({ userName, PAT }: UserDataType) {
-//   const data = await axios.put(`https://api.github.com/user/following/${userName}`, {
-//     headers: {
-//       Authorization: `token ${PAT}`,
-//     },
-//   });
-//   return data;
-// }
-
+import axios from 'axios';
 import { UserDataType } from '../types/userDataType';
 
-export function followUsers({ userName, PAT }: UserDataType) {
-  return fetch(`https://api.github.com/user/following/${userName}`, {
-    method: 'PUT',
+export async function followUsers({ userName, PAT }: UserDataType) {
+  const data = await axios.put(`https://api.github.com/user/following/${userName}`, '', {
     headers: {
       Authorization: `token ${PAT}`,
     },
   });
+  return data;
 }
-
-// export function followUsers({ userName, PAT }: UserDataType) {
-//   return axios.put(`https://api.github.com/user/following/${userName}`, {
-//     headers: {
-//       Authorization: `token ${PAT}`,
-//     },
-//   });
-// }
